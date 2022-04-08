d05f68a508755e338af32112eac3b009c5f4aca9
mitchelgitau <mitchelgitau2@gmail.com>
2022-04-08 13:36:13 -0700

    C-variables, if, else, while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,8 +10,8 @@ int main(void)
 	char letter;
 
 	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
-         	putchar('\n');
+	putchar(letter);
+       	putchar('\n');
 
 
 	return (0);
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,12 +11,12 @@ int main(void)
 
 
 	for (digit = '0'; digit <= '9'; digit++)
-		putchar(digit);
+	putchar(digit);
 
 	for (digit = 'a'; digit <= 'f'; digit++)
-		putchar(digit);
+	putchar(digit);
 
-		putchar('\n');
+	putchar('\n');
 
 
 	return (0);
