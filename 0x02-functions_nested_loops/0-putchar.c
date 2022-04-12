98c3a4fa2fc9c16edbcfdd1a89ff4b9a22ccc846
mitchelgitau <mitchelgitau2@gmail.com>
2022-04-12 12:40:54 -0700

    functions, nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ *main - Print the word "putchar"
+ * 
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	char p[] = "Holberton";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(p[i]);
+	
+	}
+          	_putchar('\n');
+
+		return (0);
+}
