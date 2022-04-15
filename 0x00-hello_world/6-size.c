0da9556a2bb1848df37900ac46f1722b4a03cd4d
mitchelgitau <mitchelgitau2@gmail.com>
2022-04-15 13:23:51 -0700

    size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *   
- *Return: Always 0 (Success)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always (0) success
+ */
 int main(void)
 {
 	char acharacter;
