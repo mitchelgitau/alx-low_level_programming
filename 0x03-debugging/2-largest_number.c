fe60741df693051a7cfdf70149e7772011781318
mitchelgitau <mitchelgitau2@gmail.com>
2022-04-23 11:16:04 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,11 +17,11 @@ int largest_number(int a, int b, int c)
 	}
 	else if (b >= a && b >= c)
 	{
-        	largest = b;
+		largest = b;
 	}
 	else
 	{
-	        largest = c;
-	}	
+		largest = c;
+	}
 	return (largest);
 }
